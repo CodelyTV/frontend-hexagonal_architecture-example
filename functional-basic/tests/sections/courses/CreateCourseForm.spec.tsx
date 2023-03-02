16a9be0c084340fcaa5fd5839f5ac672f23f7195
Nuria <soriano.nuria@gmail.com>
2023-03-02 12:18:25 +0100

    test: add tests for form errors

--- functional-basic/tests/sections/courses/CreateCourseForm.spec.tsx
+++ functional-basic/tests/sections/courses/CreateCourseForm.spec.tsx
@@ -28,4 +28,55 @@ describe("CreateCourseForm component", () => {
 
 		expect(successMessage).toBeInTheDocument();
 	});
+
+	it("displays error message if title is too short", async () => {
+		const repository = createLocalStorageCourseRepository();
+		render(
+			<CoursesContextProvider repository={repository}>
+				<CreateCourseForm />
+			</CoursesContextProvider>
+		);
+
+		const titleInput = screen.getByLabelText(/title/i);
+		userEvent.type(titleInput, "Aw");
+
+		const errorMessage = await screen.findByText("Title must be between 5 and 100 characters");
+
+		expect(errorMessage).toBeInTheDocument();
+	});
+
+	it("displays error message if title is too long", async () => {
+		const repository = createLocalStorageCourseRepository();
+		render(
+			<CoursesContextProvider repository={repository}>
+				<CreateCourseForm />
+			</CoursesContextProvider>
+		);
+
+		const titleInput = screen.getByLabelText(/title/i);
+		userEvent.type(
+			titleInput,
+			"Amet ex labore dolor amet deserunt. Tempor minim excepteur tempor dolor aute consectetur dolore. Velit Lorem nisi est cillum cupidatat sint cupidatat nostrud adipisicing eu sunt labore adipisicing minim. Sint est veniam irure fugiat ea est consectetur enim ut ipsum nostrud do duis esse. Mollit esse id tempor do deserunt eu ea nulla deserunt ut laboris qui ea."
+		);
+
+		const errorMessage = await screen.findByText("Title must be between 5 and 100 characters");
+
+		expect(errorMessage).toBeInTheDocument();
+	});
+
+	it("displays error message if image url is not a valid url", async () => {
+		const repository = createLocalStorageCourseRepository();
+		render(
+			<CoursesContextProvider repository={repository}>
+				<CreateCourseForm />
+			</CoursesContextProvider>
+		);
+
+		const imageUrlInput = screen.getByLabelText(/image/i);
+		userEvent.type(imageUrlInput, "not a valid url");
+
+		const errorMessage = await screen.findByText("Image url is not valid");
+
+		expect(errorMessage).toBeInTheDocument();
+	});
 });
